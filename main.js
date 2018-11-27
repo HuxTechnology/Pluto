f0bf9b5ccd920b600dc343a3d43dc19df49416b3
Adam Sanders <adam@hux.com>
2018-11-27 15:47:00 -0500

    Updated to use promises

--- main.js
+++ main.js
@@ -9,43 +9,54 @@ MongoClient.connect(keys.mongoConnectionURL, {useNewUrlParser: true}, function(e
 	const db = client.db('hux');
 	
 	let mailgunData = [];
+	let queryPromises = [];
 	
 	// Loop through collections
 	for (collectionName in config) {
 		collection = db.collection(collectionName);
 		
 		// Loop through queries
-		config[collectionName].forEach(qry => {
-			collection.find(qry, {limit:1}).toArray((err, docs) => {
-				if(err) return console.log("Error!", err);
-				
-				if (docs.length > 0) mailgunData.push({
-					collectionName,
-					_id: docs[0]._id,
-					qry: JSON.stringify(qry),
-				});
-			});
-		});
+		queryPromises = queryPromises.concat(
+			config[collectionName].map(qry => ({
+				promise: collection.findOne(qry),
+				qry,
+				collectionName,
+			}))
+		);
 	}
 	
-	if (mailgunData.length > 0) {
-		const MailgunInstance = Mailgun({apiKey: keys.mailgun.apiKey, domain: keys.mailgun.domain});
-		
-		let email = {
-			from: keys.mailgun.fromAddress,
-			to: keys.mailgun.toAddress,
-			subject: 'Pluto Error Found',
-			html: '',
-		};
-		
-		mailgunData.forEach(data => {
-			email.html += `Bad value found! Collection = ${data.collectionName} _id = ${data._id} qry = ${data.qry}<br>`;
-		});
+	Promise.all(queryPromises.map(item => item.promise)).then(values => {
+		values.forEach((record, index) => {
+			if (record !== null) {
+				let {collectionName, qry} = queryPromises[index];
 				
-		MailgunInstance.messages().send(email, (error, body) => {
-			if(err) return console.log("Error!", err);
+				mailgunData.push({
+					collectionName,
+					_id: record._id,
+					qry: JSON.stringify(qry),
+				});
+			}
 		});
-	}
 		
-	client.close();
+		if (mailgunData.length > 0) {
+			const MailgunInstance = Mailgun({apiKey: keys.mailgun.apiKey, domain: keys.mailgun.domain});
+			
+			let email = {
+				from: keys.mailgun.fromAddress,
+				to: keys.mailgun.toAddress,
+				subject: 'Pluto Error Found',
+				html: '',
+			};
+			
+			mailgunData.forEach(data => {
+				email.html += `Malformed record found! Collection = ${data.collectionName} _id = ${data._id} qry = ${data.qry}<br>`;
+			});
+			
+			MailgunInstance.messages().send(email, (error, body) => {
+				if(err) return console.log("Error!", err);
+			});
+		}
+		
+		client.close();
+	});
 });
