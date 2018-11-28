774783c69c527c74c7f37c333573a090ab9a8fae
Adam Sanders <adam@hux.com>
2018-11-28 10:00:02 -0500

    Revert "Revert "wrap in export for AWS""
    This reverts commit 505ef84ec41dac8853f0c9150e8d7385b971b9f6.

--- index.js
+++ index.js
@@ -3,60 +3,64 @@ const Mailgun = require("mailgun-js");
 const keys = require('./keys');
 const config = require('./config');
 
-MongoClient.connect(keys.mongoConnectionURL, {useNewUrlParser: true}, function(err, client) {
-	if(err) return console.log("Error!", err);
-	
-	const db = client.db('hux');
-	
-	let mailgunData = [];
-	let queryPromises = [];
-	
-	// Loop through collections
-	for (collectionName in config) {
-		collection = db.collection(collectionName);
+exports.handler = (event, context, callback) => {
+	MongoClient.connect(keys.mongoConnectionURL, {useNewUrlParser: true}, (err, client) => {
+		if(err) return console.log("Error!", err);
 		
-		// Loop through queries
-		queryPromises = queryPromises.concat(
-			config[collectionName].map(qry => ({
-				promise: collection.findOne(qry),
-				qry,
-				collectionName,
-			}))
-		);
-	}
-	
-	Promise.all(queryPromises.map(item => item.promise)).then(values => {
-		values.forEach((record, index) => {
-			if (record !== null) {
-				let {collectionName, qry} = queryPromises[index];
-				
-				mailgunData.push({
+		const db = client.db('hux');
+		
+		let mailgunData = [];
+		let queryPromises = [];
+		
+		// Loop through collections
+		for (collectionName in config) {
+			collection = db.collection(collectionName);
+			
+			// Loop through queries
+			queryPromises = queryPromises.concat(
+				config[collectionName].map(qry => ({
+					promise: collection.findOne(qry),
+					qry,
 					collectionName,
-					_id: record._id,
-					qry: JSON.stringify(qry),
-				});
-			}
-		});
-		
-		if (mailgunData.length > 0) {
-			const MailgunInstance = Mailgun({apiKey: keys.mailgun.apiKey, domain: keys.mailgun.domain});
-			
-			let email = {
-				from: keys.mailgun.fromAddress,
-				to: keys.mailgun.toAddress,
-				subject: 'Pluto Error Found',
-				html: '',
-			};
-			
-			mailgunData.forEach(data => {
-				email.html += `Malformed record found! Collection = ${data.collectionName} _id = ${data._id} qry = ${data.qry}<br>`;
-			});
-			
-			MailgunInstance.messages().send(email, (error, body) => {
-				if(err) return console.log("Error!", err);
-			});
+				}))
+			);
 		}
 		
-		client.close();
+		Promise.all(queryPromises.map(item => item.promise)).then(values => {
+			values.forEach((record, index) => {
+				if (record !== null) {
+					let {collectionName, qry} = queryPromises[index];
+					
+					mailgunData.push({
+						collectionName,
+						_id: record._id,
+						qry: JSON.stringify(qry),
+					});
+				}
+			});
+			
+			if (mailgunData.length > 0) {
+				const MailgunInstance = Mailgun({apiKey: keys.mailgun.apiKey, domain: keys.mailgun.domain});
+				
+				let email = {
+					from: keys.mailgun.fromAddress,
+					to: keys.mailgun.toAddress,
+					subject: 'Pluto Error Found',
+					html: '',
+				};
+				
+				mailgunData.forEach(data => {
+					email.html += `Malformed record found! Collection = ${data.collectionName} _id = ${data._id} qry = ${data.qry}<br>`;
+				});
+				
+				MailgunInstance.messages().send(email, (error, body) => {
+					if(err) return console.log("Error!", err);
+				});
+			}
+			
+			client.close();
+		});
 	});
-});
+	
+	callback(null, "success");
+}
---
+++ lambda.sh
@@ -0,0 +1,4 @@
+curl \
+--header "Authorization: 123" \
+--header "x-amz-date: 20181127T000000Z" \
+https://8okdxjqdxj.execute-api.us-east-1.amazonaws.com/default/Pluto
