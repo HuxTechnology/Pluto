eb543983c8183283f06c043bf7f19c875065f5ab
Adam Sanders <adam@hux.com>
2019-11-26 11:39:01 -0500

    You can now remotely host a config file

--- exec.sh
+++ exec.sh
@@ -1,2 +1,20 @@
 cd "$(dirname "$0")";
+
+while getopts ":u:" opt; do
+	case $opt in
+		u)
+			curl -s $OPTARG > config.js
+			;;
+		
+		\?)
+			echo "Unknown option: -$OPTARG"
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument."
+			exit 1
+			;;
+	esac
+done
+
 node index.js
