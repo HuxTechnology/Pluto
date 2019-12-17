bf965c598d7f3f86a25aab969864720bd8bb4927
Adam Sanders <adam@hux.com>
2019-12-17 15:32:18 -0500

    Revert "Don't need an sh file to run it"
    This reverts commit 14213ddfd442a60ecbac2eceb9146371d8c22c56.

---
+++ exec.sh
@@ -0,0 +1,21 @@
+cd "$(dirname "$0")";
+
+while getopts ":u:" opt; do
+	case $opt in
+		u)
+			echo "module.exports =" > config.js
+			curl -s $OPTARG >> config.js
+			;;
+		
+		\?)
+			echo "Unknown option: -$OPTARG"
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument."
+			exit 1
+			;;
+	esac
+done
+
+node index.js
